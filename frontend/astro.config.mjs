07d76fa687cd71bf5a46ec37fa16aa21ad4cd070
Leo <leozinho17silvacosta@gmail.com>
2026-04-16 19:16:35 -0700

    added: NameUser in NavBar

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server', 
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
   server: {
     port: 4321,
   },
